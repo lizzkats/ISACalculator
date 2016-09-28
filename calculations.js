8d6e3c10295afd7a84dea16f9dda1f5100993556
James D Stewart Jr <jamstewjr@gmail.com>
2016-09-27 18:31:08 -0700

    calculations started.

--- calculations.js
+++ calculations.js
@@ -1,24 +1,35 @@
 $(document).ready(function(){
-  var salary = 0;
+
   var stipend = 0;
+
   var raise = 0;
+  var salary = 0;
+
 
   var getInfo = function (id) {
     var field = document.getElementById(id).value;
-    console.log(id);
     return field;
   };
 
-  function getForm(){
+  var getLaptop = function (id){
+    var haveLaptop = $(id).is(':checked')
+  };
 
-    salary = getInfo('salary');
+  function setInfo(){
+
     stipend = getInfo('stipend');
+    laptop = getLaptop('laptop');
+    console.log('laptop', laptop)
+
     raise = getInfo('raise');
+    salary = getInfo('salary');
+      console.log('salary', salary)
+
 
   };
 
   $('#submit-form').click( function() {
-      getForm();
+    setInfo();
 
   });
 
@@ -31,5 +42,4 @@ $(document).ready(function(){
   //
   // };
 
-}
-)
+})
