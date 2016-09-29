9d401578a338df1fd4c1377f480bf836c5b07e26
Amelia Clarke <ameliavoncat@gmail.com>
2016-09-28 21:28:08 -0700

    added options to make the chart show minimum and cap correctly, but it's not calculating the cost of the stipend ISA yet

--- calculations.js
+++ calculations.js
@@ -94,108 +94,168 @@
       return cost;
   }
 
-var chartData={
-  datasets:[{
-    label: "ISA Calculator",
-    xAxisID: "salary",
-    yAxisID: "cost-years",
-    data:[{
-      x: 49999,
-      y: calculateY(49999),
-    },
-    {
-      x: 50000,
-      y: calculateY(50000),
-    },
-    {
-      x: 75000,
-      y: calculateY(75000),
-    },
-    {
-      x: 100000,
-      y: calculateY(100000),
-    },
-    {
-      x: 125000,
-      y: calculateY(125000),
-    },
-    {
-      x: 150000,
-      y: calculateY(150000),
-    },
-    {
-      x: 175000,
-      y: calculateY(175000),
-  }]
-  },
-    {
-      label: "Upfront Cost Without Laptop",
+
+  var chartData={
+    datasets:[{
+      label: "3 Year ISA Cost",
       xAxisID: "salary",
-      yAxisID: "cost-upfront",
-      data: [{
-        x: 25000,
-        y: 29750,
+      yAxisID: "cost-isa",
+      data:[{
+        x: 49999,
+        y: calculateY(49999),
+      },
+      {
+        //curve smoothing point
+        x: 50000,
+        y: 18749,
       },
       {
         x: 50000,
-        y: 29750,
+        y: calculateY(50000),
       },
       {
         x: 75000,
-        y: 29750,
+        y: calculateY(75000),
       },
+      // {
+      //   x: 80000,
+      //   y: calculateY(80000),
+      // },
+      // {
+      //   x: 90000,
+      //   y: calculateY(90000),
+      // },
       {
         x: 100000,
-        y: 29750,
+        y: calculateY(100000),
       },
       {
         x: 125000,
-        y: 29750,
+        y: calculateY(125000),
       },
       {
         x: 150000,
-        y: 29750,
+        y: calculateY(150000),
+      },
+      {
+        //cap point
+        x:(cap/(percentage+laptopPercentage+.125)/3),
+        y:cap
+      },
+      {
+        //line smoothing point
+        x: (cap/(percentage+laptopPercentage+.125)/3)-1,
+        y: cap,
       },
       {
         x: 175000,
-        y: 29750,
-      }]
-    }]
-};
-
-var options = {
-  responsive: true,
-  hoverMode: "single",
-  title: {
-    display: true,
-    text: "ISA Calculator"
-  },
-  scales: {
-    xAxes: [{
-      position: "bottom",
-      id: "salary",
-      gridLines: {
-        zeroLineColor: "#1AB898"
-      }
-    }],
-    yAxes: [{
-      type: "linear",
-      display: true,
-      position: "left",
-      id: "cost-years"
-    }, {
-      type: "linear",
-      display: false,
-      position: "right",
-      id: "cost-upfront",
-
-      gridLines: {
-        drawOnChartArea: false,
+        y: calculateY(175000),
       },
-    }],
-  }
-};
+      {
+        x:200000,
+        y: calculateY(200000)
+      }
+      ]
+    },
+      {
+        label: "Upfront Cost",
+        xAxisID: "salary",
+        yAxisID: "cost-upfront",
+        data: [
+        //   {
+        //   x: 0,
+        //   y: 29750,
+        // },
+        {
+          x: 25000,
+          y: 29750,
+        },
+        {
+          x: 50000,
+          y: 29750,
+        },
+        {
+          x: 75000,
+          y: 29750,
+        },
+        {
+          x: 100000,
+          y: 29750,
+        },
+        {
+          x: 125000,
+          y: 29750,
+        },
+        {
+          x: 150000,
+          y: 29750,
+        },
+        {
+          x: 175000,
+          y: 29750,
+        },
+        {
+          x: 200000,
+          y: 29750,
+        },
+      ]
+      }]
+  };
 
+  var options = {
+    responsive: true,
+    hoverMode: "single",
+    title: {
+      display: true,
+      text: "ISA Calculator"
+    },
+    scales: {
+      xAxes: [{
+        ticks:{
+          fixedStepSize:25000,
+          min:25000,
+          max:200000
+        },
+        position: "bottom",
+        id: "salary",
+        scaleLabel:{
+          display:true,
+          labelString:"Starting Salary",
+        },
+        gridLines: {
+          zeroLineColor: "#1AB898"
+        }
+      }],
+      yAxes: [{
+        ticks:{
+          fixedStepSize:15000,
+          min:0,
+          max:75000
+        },
+        scaleLabel:{
+          display:false,
+          labelString:"Total Cost",
+        },
+        type: "linear",
+        display: true,
+        position: "left",
+        id: "cost-isa"
+      }, {
+        ticks:{
+          fixedStepSize:15000,
+          min:0,
+          max:75000
+        },
+        type: "linear",
+        display: true,
+        position: "right",
+        id: "cost-upfront",
+        gridLines: {
+          drawOnChartArea: false,
+        },
+      }],
+    }
+  };
 $.each(chartData.datasets, function(i, dataset) {
     dataset.borderColor = "lightgrey";
     // dataset.backgroundColor = "blue";
